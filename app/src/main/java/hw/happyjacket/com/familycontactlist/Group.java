d076ce0c962b6358633e54a1ddc224501614a5cd
oleoleo <815514157@qq.com>
2016-04-17 15:08:25 +0800

    e

---
+++ app/src/main/java/hw/happyjacket/com/familycontactlist/Group.java
@@ -0,0 +1,13 @@
+package hw.happyjacket.com.familycontactlist;
+
+/**
+ * Created by leo on 2016/4/10.
+ */
+public class Group {
+    public int _id;
+    public String name;
+    public String mobilephone;
+    public String photo ;
+    public String group ;
+    public String info;
+}
